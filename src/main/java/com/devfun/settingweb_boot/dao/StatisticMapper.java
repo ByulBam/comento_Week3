2d77f410fd53be5a2260431c8c9f0df349b9d979
Kim, jeonghwan <jhkim980214@gmail.com>
2023-06-09 21:10:08 +0900

    월별, 일별 접속자수 추가

--- src/main/java/com/devfun/settingweb_boot/dao/StatisticMapper.java
+++ src/main/java/com/devfun/settingweb_boot/dao/StatisticMapper.java
@@ -1,7 +1,8 @@
 package com.devfun.settingweb_boot.dao;
 import java.util.HashMap;
- 
+
 public interface  StatisticMapper {
-    public HashMap<String, Object> selectYearLogin(String year);
- 
+	public HashMap<String, Object> selectYearLogin(String year);
+	public HashMap<String, Object> selectMonthLogin(String yearMonth);
+	public HashMap<String, Object> selectDayLogin(String daily);
 }

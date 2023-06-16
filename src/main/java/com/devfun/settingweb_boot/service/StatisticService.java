f5e5822aa11e00d3706b4ab3c5bf9b3ccdd83002
Kim, jeonghwan <jhkim980214@gmail.com>
2023-06-16 22:53:01 +0900

    4주차 추가

--- src/main/java/com/devfun/settingweb_boot/service/StatisticService.java
+++ src/main/java/com/devfun/settingweb_boot/service/StatisticService.java
@@ -4,5 +4,8 @@ import java.util.HashMap;
  
 public interface StatisticService {
     public HashMap<String,Object> yearloginNum (String year);
+    public HashMap<String,Object> monthloginNum (String yearMonth);
+    public HashMap<String,Object> dailyloginNum (String daily);
+   
     
 }
--- src/main/java/com/devfun/settingweb_boot/service/StatisticServiceImpl.java
+++ src/main/java/com/devfun/settingweb_boot/service/StatisticServiceImpl.java
@@ -32,5 +32,43 @@ public class StatisticServiceImpl implements StatisticService {
         
         return retVal;
     }
+    
+    public HashMap<String, Object> monthloginNum (String yearMonth) {
+        // TODO Auto-generated method stub
+        HashMap<String, Object> retVal = new HashMap<String,Object>();
+        
+        try {
+            retVal = uMapper.selectYearLogin(yearMonth);
+            retVal.put("yearMonth", yearMonth);
+            retVal.put("is_success", true);
+            
+        }catch(Exception e) {
+            retVal.put("totCnt", -999);
+            retVal.put("yearMonth", yearMonth);
+            retVal.put("is_success", false);
+        }
+        
+        return retVal;
+    }
+    
+    public HashMap<String, Object> dailyloginNum (String daily) {
+        // TODO Auto-generated method stub
+        HashMap<String, Object> retVal = new HashMap<String,Object>();
+        
+        try {
+            retVal = uMapper.selectYearLogin(daily);
+            retVal.put("daily", daily);
+            retVal.put("is_success", true);
+            
+        }catch(Exception e) {
+            retVal.put("totCnt", -999);
+            retVal.put("daily", daily);
+            retVal.put("is_success", false);
+        }
+        
+        return retVal;
+    }
+    
+    
  
 }
